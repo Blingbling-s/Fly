5ab24f3139ec7b0e6c4424a1cf6a8c53d3808ff0
RepainZeng <bu4767@gmail.com>
2017-05-14 21:30:52 +0800

    commit

---
+++ src/fly/Award.java
@@ -0,0 +1,10 @@
+package fly;
+
+public interface Award {
+
+    int DOUBLE_FIRE = 0;
+    int LIFE = 1;
+
+    int getType();
+
+}
---
+++ src/fly/Enemy.java
@@ -0,0 +1,5 @@
+package fly;
+
+public interface Enemy {
+    int getScore();
+}
